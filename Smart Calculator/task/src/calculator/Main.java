960fd2d85d48ebbeda776a04c67af5d4a7c3d5f7
ahmedhosnypro <80482137+ahmedhosnypro@users.noreply.github.com>
2022-03-31 21:17:55 +0200

    2+2

---
+++ Smart Calculator/task/src/calculator/Main.java
@@ -0,0 +1,13 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n1 = Integer.parseInt(scanner.next());
+        int n2 = Integer.parseInt(scanner.next());
+        System.out.println(n1 + n2);
+    }
+}
